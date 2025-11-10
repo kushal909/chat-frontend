0d5e299f1f7d6f8796413df193cf4259000809f9
kushal909 <kushalukumar909@gmail.com>
2025-11-10 21:02:23 +0530

    updated frontend react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://65.2.81.219:5000/");
 
 function App() {
   const [username, setUsername] = useState("");
